f5dbb875c4dcf735b68dfd0f86793dae9676d482
Tiaan Steyn <tsanalytics@outlook.com>
2023-03-12 09:23:38 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Up") {
+        if (angle > 10) {
+            maqueen.servoRun(maqueen.Servos.S1, angle)
+            angle += -3
+            basic.pause(100)
+        }
+    } else if (receivedString == "Down") {
+        if (angle < 170) {
+            maqueen.servoRun(maqueen.Servos.S1, angle)
+            angle += 3
+            basic.pause(100)
+        }
+    } else if (receivedString == "Stop") {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "F") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Math.map(value, 550, 1024, 10, 255))
+    } else if (name == "B") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, Math.map(value, 1, 450, 255, 10))
+    } else if (name == "L") {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Math.map(value, 1, 450, 255, 40))
+    } else if (name == "R") {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Math.map(value, 550, 1024, 40, 255))
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    }
+})
+let angle = 0
+radio.setGroup(1)
+angle = 90
+maqueen.servoRun(maqueen.Servos.S1, angle)
